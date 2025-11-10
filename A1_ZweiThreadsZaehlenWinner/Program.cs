67a01a69fa0160a014378ed5cb80848b2369da63
Samuelk0nrad <samuel.ko702@gmail.com>
2025-11-10 11:01:53 +0400

    a11

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,22 +5,41 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-   
+
+    static int counter = 0;
+    static int counterDown = 100;
     
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
         
-        
+        Thread threadA = new Thread(CountUpThreadA);
+        Thread threadB = new Thread(CountDownThreadB);
+
+        threadA.Start();
+        threadB.Start();
+
+        threadA.Join();
+        threadB.Join();
     }
     
     private static void CountUpThreadA()
     {
+        for (;counter < 100; counter++)
+        {
+            Console.WriteLine($"Thread A: {counter}");
+            Thread.Sleep(100);
+        }
         
     }
     
     private static void CountDownThreadB()
     {
-       
+        for (;counterDown > 0; counterDown--)
+        {
+            counterDown--;
+            Console.WriteLine($"Thread B: {counterDown}");
+            Thread.Sleep(100);
+        }
     }
 }
